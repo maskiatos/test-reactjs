b83216fb3b2e6ce1dd27d6890c272513075d153b
ChrisVu <maskiatos@gmail.com>
2022-08-14 17:45:51 +0700

    them component con UserInfor qua UserInfo.js

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,34 +1,17 @@
+//ĐÂY LÀ COMPONENT CON CỦA App.js VÀ ĐƯỢC GỌI RA BÊN ĐÓ BẰNG CÁCH EXPORT RA ĐỂ CHIA CODE RA CHO DỄ QUẢN LÝ
 import React from "react" //import thư viện react đã khai báo bên package.json
+import UserInfor from "./UserInfo" //import componet UserInfor export ra từ UserInfo.js
 
 class MyComponent extends React.Component { //khai báo class MyComponent kế thừa Component từ thư viện React vừa import
     //JSX (JavaScript XML), chỉ riêng reactJS có cái này, là extension cho phép gõ html trực tiếp trong Reactjs
-    state = {   
-        name: "Thinh",
-        age: 29,
-        address: "Ha Noi"
-    }
-    handleClick(event){ //dưới là onClick={(x) => {this.handleClick(x)}} thì nó mới hiểu cái this là gì và chạy, không là báo lỗi 
-        this.setState({     //setState() là hàm đặt lại thuộc tính, truyền vào OBJECT rồi chỉnh
-            name: "Chris"
-        })  //RIÊNG class component CÓ CHỨC NĂNG MERGE STATE (GỘP CÁC STATE KHI CÓ NHIỀU STATE), function component KHÔNG CÓ
-        console.log("My name is ",this.state.name)  //this ở đây là MyComponent 
-    }
-    handleClick1 = (event) =>{ /*nếu dưới chỉ gọi onClick={this.handleClick1} thì ở đây phải viết kiểu arrow function như này 
-    thì nó mới hiểu cái this ở đây, không sẽ báo lỗi*/
-        console.log("My name is ",this.state.name)  
-    }
-    handleOnMouseOver(x){   //x ở đây cũng giống event ở trên, là biến chứa thông tin sự kiện
-        console.log(x.pageX)    //lấy ra hoàng độ điểm hover vào
-    }
+    
     render(){ //component bắt buộc ít nhất phải có render()
         //JSX chỉ cho render 1 code block thôi, ví dụ ở đây nếu có 1 <div> khác song song thì báo lỗi, nested <div> thì được
         return(     
         //nên viết theo cách button Click me ở dưới để không phải đổi handleClick(event) thành arrowfunction để chạy cái this.state.name
             <div>
-                my first component, {this.state.name}, {this.state.age} years old 
-                <button onClick={(x) => {this.handleClick(x)}}>Click me (Arrow function)</button>   {/* nên dùng cách này */}
-                <button onClick={this.handleClick1}>Click me (normal)</button>
-                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+                <UserInfor/>    
+                {/*gọi UserInfor import từ bên UserInfo.js, website vẫn chạy bình thường như khi viết hết code trong đấy ở file này*/}
             </div>    
         )
     }
---
+++ src/components/UserInfo.js
@@ -0,0 +1,58 @@
+//ĐÂY LÀ COMPONENT CON CỦA MyComponent.js VÀ ĐƯỢC GỌI RA BÊN ĐÓ BẰNG CÁCH EXPORT RA ĐỂ CHIA CODE RA XỬ LÝ form
+import React from "react"
+
+class UserInfor extends React.Component{
+    state = {   
+        name: "Thinh",
+        age: 29,
+        address: "Ha Noi"
+    }
+    handleClick(a){ //dưới là onClick={(x) => {this.handleClick(x)}} thì nó mới hiểu cái this là gì và chạy, không là báo lỗi 
+        this.setState({     //setState() là hàm đặt lại thuộc tính, truyền vào OBJECT rồi chỉnh, hạn chế dùng ngoài function (NÊN VIẾT NTN)
+            name: "Chris",
+            age: Math.floor((Math.random()*100)+1)
+        })  //RIÊNG class component CÓ CHỨC NĂNG MERGE STATE (GỘP CÁC STATE KHI CÓ NHIỀU STATE), function component KHÔNG CÓ
+        //setState() Ở TRÊN CŨNG TƯƠNG TỰ this.state.age=a.target.value NHƯNG KHÔNG NÊN DÙNG CÁI NÀY VÌ DỄ SINH RA BUG
+        console.log("My name is ",this.state.name)  //this ở đây là MyComponent 
+    }
+    handleClick1 = (event) =>{ /*nếu dưới chỉ gọi onClick={this.handleClick1} thì ở đây phải viết kiểu arrow function như này 
+    thì nó mới hiểu cái this ở đây, không sẽ báo lỗi*/
+        console.log("My name is ",this.state.name)  
+    }
+    handleOnMouseOver(x){   //x ở đây cũng giống event ở trên, là biến chứa thông tin sự kiện
+        console.log(x.pageX)    //lấy ra hoành độ điểm hover vào
+    }
+    handleOnChangeName(event){
+        this.setState({
+            name: event.target.value, //event là biến chứa hành động của người dùng, nó là 1 object có target là <input> gọi nó
+        })
+    }
+    handleOnChangeAge(event){
+        this.setState({
+            age: event.target.value, //event là biến chứa hành động của người dùng, nó là 1 object có target là <input> gọi nó
+        })
+    }
+    handleSubmit(event){
+        event.preventDefault()  //ngăn chặn hiệu ứng mặc định, SUBMIT thì nó sẽ mặc định sau khi bấm sẽ reload lại, giờ thì không
+        console.log(this.state)
+    }
+    render(){
+        return(
+            <div>
+                my first component, {this.state.name}, {this.state.age} years old 
+                <button onClick={(x) => this.handleClick(x)}>Click me (Arrow func)</button>   {/*nên dùng, trên chỉ cần handleClick(a){}*/}
+                <button onClick={this.handleClick1}>Click me (normal)</button>
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+                <form onSubmit={(x) => this.handleSubmit(x)}>
+                    <label>Name</label>
+                    <input type="text" onChange={(x) => this.handleOnChangeName(x)} value={this.state.name}/> 
+                        {/*nên để Reactjs quản lý, VALUE như này mới chạy, nếu gán thì sẽ không chạy đúng ý được và hiệu năng kém hơn*/}
+                    <label>Age</label>
+                    <input type="text" onChange={(x) => this.handleOnChangeAge(x)} value={this.state.age}/> 
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+export default UserInfor
